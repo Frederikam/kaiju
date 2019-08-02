1687d2643b7c8672373697029f2f5daff50ac757
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-08-02 12:07:47 +0200

    Fix header warning

--- include/output.h
+++ include/output.h
@@ -1,3 +1,4 @@
 #pragma once
+#include <wayland-server-core.h>
 void output_destroy_notify(struct wl_listener *listener, void *data);
 void new_output_notify(struct wl_listener *listener, void *data);
